a2d4d37f6cdfd27d3ced5cdd8b5527f1140ccd44
usnikchawla <34809521+usnikchawla@users.noreply.github.com>
2019-04-09 20:54:10 +0530

    Add files via upload

---
+++ sketch_jan20a.ino
@@ -0,0 +1,23 @@
+#include <Servo.h>
+Servo myobject;
+int pos;
+
+void setup() {
+  myobject.attach(9);
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  for(pos=0;pos<=180;pos++){
+    myobject.write(pos);
+    Serial.println(pos);
+    delay(15);
+  }
+  for(pos=180;pos>=0;pos--){
+    myobject.write(pos);
+    Serial.println(pos);
+    delay(15);
+  }
+ 
+}
